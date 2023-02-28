26a994bae10f65c549c67b146fac048748de635e
AnnaHsu1 <annahsu2000@hotmail.com>
2023-02-28 18:38:57 -0500

    More CSS fixes

--- src/client/src/components/EditUserProfile.jsx
+++ src/client/src/components/EditUserProfile.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "../css/EditUserProfile.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { TextField } from "@mui/material";
+import Button from '@mui/material/Button' 
 
 const EditUserProfile = () => {
   const [languages, setLanguages] = useState([]);
@@ -92,83 +94,110 @@ const EditUserProfile = () => {
       <form className="userProfileForm" onSubmit={submitEditProfile}>
       <h1 className="edit-profile-title">Edit your profile</h1>
         <div className="EditUserForm-Container">
-          <div className="edit-right-side">
-          <label>
+          <div className="edit-left-side">
+          {/* <label>
             Email
             <input type="email" value={userData.email} onChange={e => setUserData({ ...userData, email: e.target.value })} />
-          </label>
+          </label> */}
+          <TextField id="editField" label="Email" variant="outlined" onChange={e => setUserData({ ...userData, email: e.target.value })} />
           <br />
-          <label>
+          {/* <label>
             Bio
             <textarea rows="3" value={userData.bio} onChange={e => setUserData({ ...userData, bio: e.target.value })} />
-          </label>
+          </label> */}
+          <TextField id="editField" label="Bio" variant="outlined" onChange={e => setUserData({ ...userData, bio: e.target.value })}/>
           <br />
-          <div className="list-button">
-            <button type="button" onClick={workExpChange}>
-              Add
-            </button>
-          </div>
-          <label>
+          <div className="button-field">
+
+          {/* <label>
             Work Experience
             <input type="text" value={workExp} onChange={e => setWorkExp(e.target.value)} />
-          </label>
-          <br />
+          </label> */}
+          <TextField id="editField" value = {workExp} label="Work Experience" variant="outlined" onChange={e => setWorkExp(e.target.value)}/>
           <div className="list-button">
             <button type="button" onClick={educationChange}>
               Add
             </button>
           </div>
-          <label>
+          </div>
+          <br />
+          <div className="button-field">
+          {/* <label>
             Education
             <input type="text" value={education} onChange={e => setEducation(e.target.value)} />
-          </label>
+          </label> */}
+            <TextField id="editField" value={education} label="Education" variant="outlined" onChange={e => setEducation(e.target.value)}/>
+            <div className="list-button">
+              <button type="button" onClick={educationChange}>
+                Add
+              </button>
+            </div>
+          </div>
           <br />
           </div>
-          <div className="edit-left-side">
-          <div className="list-button">
-            <button type="button" onClick={skillsChange}>
-              Add
-            </button>
+          <div className="edit-right-side">
+            <div className="button-field">
+
+            {/* <label>
+              Skills
+              <input type="text" value={skills} onChange={e => setSkills(e.target.value)} />
+            </label> */}
+              <TextField id="editField" value={skills} label="Skills" variant="outlined" onChange={e => setSkills(e.target.value)} />
+              <div className="list-button">
+                <button type="button" onClick={skillsChange}>
+                  Add
+                </button>
+              </div>
+            </div>
+            <br />
+            <div className="button-field">
+
+            {/* <label>
+              Languages
+              <input type="text" value={languages} onChange={e => setLanguages(e.target.value)} />
+            </label> */}
+            <TextField id="editField" value={languages} label="Languages" variant="outlined" onChange={e => setLanguages(e.target.value)}/>
+            <div className="list-button">
+              <button type="button" onClick={languagesChange}>
+                Add
+              </button>
+            </div>
+            </div>
+            <br />
+            <div className="button-field">
+
+            {/* <label>
+              Volunteering
+              <textarea rows="3" value={volunteering} onChange={e => setVolunteering(e.target.value)} />
+            </label> */}
+            <TextField id="editField" value={volunteering} label="Volunteering" variant="outlined" onChange={e => setVolunteering(e.target.value)}/>
+            <div className="list-button">
+              <button type="button" onClick={volunteeringChange}>
+                Add
+              </button>
+            </div>
+            </div>
+            <br />
+            {/* <label>
+              Resume
+              <input type="file" accept=".pdf" onChange={handleResumeChange} />
+            </label> */}
+            <div className="editFile">
+              <Button variant="contained" component="label" style={{width: "45%"}}> Upload Resume<input hidden accept="image/*" multiple type="file" onChange={handleResumeChange}/></Button>
+              <br />
+              {/* <label>
+                Cover Letter
+                <input type="file" accept=".pdf" onChange={handleCoverLetterChange} />
+              </label> */}
+              {/* <TextField id="editField" type ="file" variant="outlined" /> */}
+              <Button variant="contained" component="label" style={{width: "45%"}}> Upload Cover Letter<input hidden accept="image/*" multiple type="file" onChange={handleCoverLetterChange}/></Button>
+            </div>
+
+            </div>      
           </div>
-          <label>
-            Skills
-            <input type="text" value={skills} onChange={e => setSkills(e.target.value)} />
-          </label>
-          <br />
-          <div className="list-button">
-            <button type="button" onClick={languagesChange}>
-              Add
-            </button>
+          <div className="submit-button">
+            <button type="submit">Save changes</button>
           </div>
-          <label>
-            Languages
-            <input type="text" value={languages} onChange={e => setLanguages(e.target.value)} />
-          </label>
-          <br />
-          <div className="list-button">
-            <button type="button" onClick={volunteeringChange}>
-              Add
-            </button>
-          </div>
-          <label>
-            Volunteering
-            <textarea rows="3" value={volunteering} onChange={e => setVolunteering(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Resume
-            <input type="file" accept=".pdf" onChange={handleResumeChange} />
-          </label>
-          <br />
-          <label>
-            Cover Letter
-            <input type="file" accept=".pdf" onChange={handleCoverLetterChange} />
-          </label>
-          </div>      
-        </div>
-        <div className="submit-button">
-          <button type="submit">Save changes</button>
-        </div>
       </form>
     </div>
   );
